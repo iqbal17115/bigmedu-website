3c3c12b72a1668186b7ed5435dcebb15f356887a
iqbal17115 <iqbal86571361@gmail.com>
2025-03-07 20:12:19 +0600

    remove unused code

--- resources/views/backend11/user-management/user-info/email.blade.php
+++
@@ -1,6 +0,0 @@
-<p>Your Information:</p>
-<div style="padding-left: 50px;">
-	<p>Email: {{$email}}</p>
-	<p>Password: {{$code}}</p>
-</div>
-<p><strong>You can can login with this email and password, Thank you</strong></p>
